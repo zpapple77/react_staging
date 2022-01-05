7dd103fd5693ef6b57edfcfd6c3cb2ec03d3feed
Pinger <947384988@qq.com>
2022-01-05 23:42:02 +0800

    新增路由的学习

--- 08_src_路由的基本使用/App.jsx
+++ 08_src_路由的基本使用/App.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
-import { NavLink, Route,Routes } from 'react-router-dom'
-import About from './pages/About' //About是路由组件
-import Home from './pages/Home' //Home是路由组件
-import Header from './components/Header' //Header是一般组件
+import { Link, Route,Routes } from 'react-router-dom'
+import About from './Components/About'
+import Home from './Components/Home';
 export default class App extends Component {
     render() {
         return (
             <div>
                 <div className="row">
                     <div className="col-xs-offset-2 col-xs-8">
-                        <Header/>
+                        <div className="page-header"><h2>React Router Demo</h2></div>
                     </div>
                 </div>
                 <div className="row">
@@ -20,8 +19,8 @@ export default class App extends Component {
                             <a className="list-group-item active" href="./home.html">Home</a> */}
 
                             {/* 在React中靠路由链接实现切换组件 */}
-                            <NavLink activeclassname = "demo" className="list-group-item" to="/about">About</NavLink>
-                            <NavLink activeclassname = "demo" className="list-group-item" to="/home">Home</NavLink>
+                            <Link className="list-group-item" to="/about">About</Link>
+                            <Link className="list-group-item" to="/home">Home</Link>
                         </div>
                     </div>
                     <div className="col-xs-6">
